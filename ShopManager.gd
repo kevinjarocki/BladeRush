c7d64b5ec41aa2f525caf393846fd87615034c7f
DanielDomaracki <danieldomaracki@gmail.com>
2024-04-13 15:37:08 -0400

    added ingot searching

--- ShopManager.gd
+++ ShopManager.gd
@@ -5,11 +5,54 @@ extends Node2D
 
 
 func _on_player_interacted(station):
+	
+	#If player is at an interactable station -> Go to a function for each station
 	if station:
 		print(station.owner.name)
+		
+		if station.owner.name == "Anvil":
+			playerAtAnvil()
+		elif station.owner.name == "Forge":
+			playerAtForge()
+		elif station.owner.name == "OreBox":
+			playerAtOreBox()
+		elif station.owner.name == "CashRegister":
+			playerAtCashRegister()
+			
 	else:
 		print("No station nearby")
 	
-	if station.owner.name == "Anvil":
+
+	
+func playerAtAnvil():
+	pass
+	
+func playerAtForge():
+	if ingotCheck($Anvil):
+		print("Cant add another ingot, ")
+	
+	if ingotCheck($Player):
+		ingotCheck($Player)
+				
 		
-		
+	else:
+		ingotCheck($Player)
+	
+func playerAtOreBox():
+	if !ingotCheck($Player):
+		var item = load("res://ingot.tscn").instantiate()
+		$Player.add_child(item)
+		print ("Picked up ingot")
+	
+	else:
+		print ("Player already holding ingot")
+	
+func playerAtCashRegister():
+	pass
+
+#Checks if player is holding an ingot, returns ingot node or false
+func ingotCheck(node):
+	for child in node.get_children():
+		if child.name == "Ingot":
+			return child
+	return false

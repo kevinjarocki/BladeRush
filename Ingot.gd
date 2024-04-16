c80f84ed12ed10cba22230425bbcda8616f563f0
ForgedInCode <166935741+ForgedInCode@users.noreply.github.com>
2024-04-16 19:51:51 -0400

    INGOT IS PRETY

--- Ingot.gd
+++ Ingot.gd
@@ -19,22 +19,35 @@ var materialProperties = {
 	}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print($ColorRect.color)
+	#print($ColorRect.color)
 	var stage = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var WhiteHot = Color(0.229,0.202,0.002,0.250)
 	var ColdMetal = Color(0.0150,0,0,0.198)
-	var MetalGlow = ColdMetal + Color(.146/500 * temperature, .202/2000*temperature, .002/1000*temperature, .100/1000*temperature)
-	$ColorRect.set_color(MetalGlow)
-	
+	var MetalGlow = ColdMetal + Color(.25/1000 * 1.1 *temperature, .25/1000*0.1*pow(temperature,1.2) - 0.25/1000*0.2*temperature, 0 , .202/1000*0.5*temperature)
+	#self_modulate.r = 1
+	#self_modulate.g = .202/2000*temperature
+	#self_modulate.b = 0
+	#self_modulate.a = 1
+	#self_modulate = MetalGlow
+	#print(self_modulate)
+	$AnimatedSprite2D.self_modulate = Color(1,1,1,1)
+
+	$Filter.self_modulate = MetalGlow
+	#$ColorRect.set_color(MetalGlow)
+	print(temperature)
+	print("filter", $Filter.self_modulate)
+	print("base", $AnimatedSprite2D.self_modulate)
 	if isForge:
-		if temperature < 15000:
+		if temperature < 8000:
 			temperature += materialProperties["heatRate"]
 	else:
-		if temperature > 0:
+		if temperature >= materialProperties["coolRate"]:
 			temperature -= materialProperties["coolRate"]
+		else:
+			temperature = 0
 
 
 

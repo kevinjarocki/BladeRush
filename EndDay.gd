1a7519a9e8dcb4896ea516de11eb7cb98f20f98e
DanielDomaracki <danieldomaracki@gmail.com>
2024-04-20 11:55:01 -0400

    We got MODS

--- EndDay.gd
+++ EndDay.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 signal nextDayPressed
+signal speedInc
+signal coolingDec
+signal heatInc
 
 func endDay(day, money):
 	visible = true
@@ -9,3 +12,12 @@ func endDay(day, money):
 func _on_next_day_pressed():
 	visible = false
 	nextDayPressed.emit()
+
+func _on_player_speed_inc_pressed():
+	speedInc.emit()
+
+func _on_cooling_rate_dec_pressed():
+	coolingDec.emit()
+
+func _on_heat_rate_inc_pressed():
+	heatInc.emit()
